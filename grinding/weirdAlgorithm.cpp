2464764e714f61b34fbbaac294e7e00efdd84ee8
Mohammad Abdul Quddus Salam <quddussalam555@gmail.com>
2026-03-02 07:56:38 +0700

    CSES: Weird Algorithm

---
+++ grinding/weirdAlgorithm.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long n;
+    cin >> n;
+
+    cout << n << " ";
+
+    while (n != 1) {
+        n = n % 2 == 0 ? n / 2 : n * 3 + 1;
+        cout << n << " ";
+    }
+
+    return 0;
+
+}
