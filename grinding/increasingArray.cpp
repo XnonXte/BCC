d8c3791a963bddb557331739ef99c220788460fc
Mohammad Abdul Quddus Salam <quddussalam555@gmail.com>
2026-03-04 19:10:43 +0700

    CSES: Increasing Array

---
+++ grinding/increasingArray.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int n; cin >> n;
+    vector<long long> v(n);
+    
+    for (size_t i = 0; i < n; i++) {
+        cin >> v[i]; 
+    }
+ 
+    long long tmp = v[0];
+    long long ans = 0;
+ 
+    for (int i = 1; i < n; i++) {
+        if (v[i] < tmp) {
+            int diff = tmp - v[i];
+            ans += diff;
+            v[i] += diff;
+        }
+        tmp = v[i];
+    }
+ 
+    cout << ans << endl;
+ 
+    return 0;
+}
