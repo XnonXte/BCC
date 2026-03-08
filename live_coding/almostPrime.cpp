5b76624c9c8100af8bdd99da9c21923f21216041
Mohammad Abdul Quddus Salam <quddussalam555@gmail.com>
2026-03-08 11:47:49 +0700

    Codeforces: Almost Prime

--- live_coding/almostPrime.cpp
+++ live_coding/almostPrime.cpp
@@ -3,26 +3,36 @@ using namespace std;
 
 vector<int> sieveOfEratosthenes(int n)
 {
-    bool is_prime[n + 1];
-    memset(is_prime, true, sizeof(is_prime));
-    is_prime[0] = is_prime[1] = false;
+    bool isPrime[n + 1];
+    memset(isPrime, true, sizeof(isPrime));
+    isPrime[0] = isPrime[1] = false;
     vector<int> primeNumbers;
 
-    for (int p = 2; p * p <= n; p++) {
-        if (is_prime[p]) {
-            for (int i = p * p; i <= n; i += p) {
-                is_prime[i] = false;
+    for (int i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
             }
         }
     }
 
     for (int i = 2; i <= n; i++) {
-        if (is_prime[i]) {
+        if (isPrime[i]) {
             primeNumbers.push_back(i);
         }
     }
 
     return primeNumbers;
+}
+
+bool checkIsAlmostPrime(vector<int> primeNumber, int n) {
+    int cnt = 0;
+    for (auto num : primeNumber) {
+        if (n % num == 0) {
+            cnt++;
+        }
+    }
+    return cnt == 2;
 }
 
 int main() {
@@ -33,17 +43,9 @@ int main() {
     int ans = 0;
 
     for (int i = 1; i <= n; i++) {
-        vector<int> primeNumbers = sieveOfEratosthenes(i);
-        int cnt = 0;
-        for (int j = 0; j < primeNumbers.size(); j++) {
-            if (cnt == 2) {
-                ans++;
-                cnt = 0;
-            }
-            if (i % primeNumbers[j] == 0) {
-                cnt++;
-            }
-        }      
+        if (checkIsAlmostPrime(sieveOfEratosthenes(n), i)) {
+            ans++;
+        }
     }
 
     cout << ans << endl;
